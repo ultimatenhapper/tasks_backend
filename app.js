ba25159269c408212da89d0864851ccad9b14e09
ultimatenhapper <belikemike3057@hotmail.com>
2023-12-23 20:46:01 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.status(200).send('Hello from the server side')
+});
+
+app.post('/', (req, res) => {
+    res.send('You can post to this endpoint...')
+}); 
+
+const port = 3000;
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`)
+});
