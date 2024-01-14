e2cb690709b5beab6a9db0eef2a9d7ab167b1074
Javi <belikemike3057@hotmail.com>
2024-01-14 08:09:40 +0100

    Add environment variables

--- app.js
+++ app.js
@@ -6,7 +6,10 @@ const userRouter = require('./routes/userRouter');
 
 const app = express();
 
-app.use(morgan('dev'));
+if(process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
