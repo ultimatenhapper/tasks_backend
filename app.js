d70383bec92a5690c18d9ff536bc4ee320cf4ce3
Javi <belikemike3057@hotmail.com>
2024-02-12 12:03:11 +0100

    Adding sorting, limiting, fields filtering, pagination to Task

--- app.js
+++ app.js
@@ -13,6 +13,11 @@ if (process.env.NODE_ENV === 'development') {
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
+
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/tasks', taskRouter);
 
